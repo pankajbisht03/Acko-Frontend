bf454b1911e27d20394948afb5d39b023f375d02
Vaibhav Solanki <Vaibhavdasss@gmail.com>
2022-03-31 03:26:28 +0530

    fix navbar on all tab

--- src/Components/Routers/Router.jsx
+++ src/Components/Routers/Router.jsx
@@ -1,12 +1,10 @@
 import { Routes, Route } from "react-router-dom";
 import Home from "../home/Home";
-import Navbar from "../Navbar/navbar";
 import NotFound from "../NotFound/NotFound";
 
 export default function Router() {
   return (
     <>
-      <Navbar />
       <Routes>
         <Route path="/" element={<Home />} />
         <Route path="/*" element={<NotFound />} />
--- src/Components/home/Home.jsx
+++ src/Components/home/Home.jsx
@@ -2,13 +2,17 @@ import Intro from "./components/Intro/Intro";
 import Ratings from "./components/Ratings/Ratings";
 import PartnerPolicy from "./components/PartnerPolicy/PartnerPolicy";
 import Feature from "./components/feature/feature";
+import Navbar from "./components/Navbar/navbar";
 export default function Home() {
   return (
-    <main>
-      <Intro />
-      <Ratings />
-      <PartnerPolicy />
-      <Feature />
-    </main>
+    <>
+      <Navbar />
+      <main>
+        <Intro />
+        <Ratings />
+        <PartnerPolicy />
+        <Feature />
+      </main>
+    </>
   );
 }
