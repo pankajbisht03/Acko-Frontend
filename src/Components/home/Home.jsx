0d3abe5ebe4d043de501ea3d9d4d368780bf4b40
Vaibhav Solanki <Vaibhavdasss@gmail.com>
2022-03-30 08:41:27 +0530

    salakha final

--- src/Components/home/Home.jsx
+++ src/Components/home/Home.jsx
@@ -1,8 +1,12 @@
 import Intro from "./components/Intro/Intro";
+import Ratings from "./components/Ratings/Ratings";
+import PartnerPolicy from "./components/PartnerPolicy/PartnerPolicy";
 export default function Home() {
   return (
     <main>
       <Intro />
+      <Ratings />
+      <PartnerPolicy />
     </main>
   );
 }
