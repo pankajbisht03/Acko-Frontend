f7ea373d3330bc0e21cd6c2bd2c60f573c24c481
Vaibhav Solanki <Vaibhavdasss@gmail.com>
2022-03-30 07:30:42 +0530

    added Change logo

--- src/Components/home/components/Intro.jsx
+++ src/Components/home/components/Intro.jsx
@@ -1,7 +1,15 @@
 import { FaCar, FaBiking, FaHeartbeat } from "react-icons/fa";
+import { useState } from "react";
 import "./intro.css";
 
 export default function Intro() {
+  let active =
+    "d-flex align-items-center justify-content-center flex-column w-100 BlockIntro activeBlockIntro";
+  const [first, setfirst] = useState({
+    car: true,
+    bike: false,
+    personal: false,
+  });
   return (
     <div className="container-fluid" id="introMblock">
       <div className="first text-center">
@@ -17,19 +25,40 @@ export default function Intro() {
         </div>
         <div className="centreCont">
           <div className="centre d-flex align-content-center align-items-center justify-content-around my-3">
-            <div className="d-flex align-items-center justify-content-center flex-column w-100 BlockIntro activeBlockIntro">
+            <div
+              className={
+                first.car
+                  ? active
+                  : "d-flex align-items-center justify-content-center flex-column w-100 BlockIntro"
+              }
+              onClick={() => setfirst({ car: true })}
+            >
               <span className="imgSpanBlock">
                 <FaCar />
               </span>
               <p>Car &amp; Taxi</p>
             </div>
-            <div className="d-flex align-items-center justify-content-center flex-column w-100 BlockIntro">
+            <div
+              className={
+                first.bike
+                  ? active
+                  : "d-flex align-items-center justify-content-center flex-column w-100 BlockIntro"
+              }
+              onClick={() => setfirst({ bike: true })}
+            >
               <span className="imgSpanBlock">
                 <FaBiking />
               </span>
               <p>Bike</p>
             </div>
-            <div className="d-flex align-items-center justify-content-center flex-column w-100 BlockIntro">
+            <div
+              className={
+                first.personal
+                  ? active
+                  : "d-flex align-items-center justify-content-center flex-column w-100 BlockIntro"
+              }
+              onClick={() => setfirst({ personal: true })}
+            >
               <span className="imgSpanBlock">
                 <FaHeartbeat />
               </span>
@@ -37,14 +66,29 @@ export default function Intro() {
             </div>
           </div>
           <div className="carNumber d-flex align-items-center bg-white px-3 border border-secondary rounded">
-            <img
+            <div className="logoIn">
+              {first.car ? (
+                <FaCar />
+              ) : first.bike ? (
+                <FaBiking />
+              ) : (
+                <FaHeartbeat />
+              )}
+            </div>
+            {/* <img
               src="data:image/svg+xml,%3Csvg width='28' height='16' viewBox='0 0 28 16' fill='none' xmlns='http://www.w3.org/2000/svg'%3E%3Cpath d='M24.9857 5.46451L20.8275 4.49938L17.3533 1.30496C16.4411 0.462173 15.2607 0 14.0267 0H9.06355C7.61486 0 6.28689 0.720445 5.50889 1.94384L3.83216 4.54017L2.59809 4.82562C1.47133 5.0839 0.666504 6.1034 0.666504 7.28601V10.1542C0.666504 11.4728 1.64571 12.5602 2.96027 12.669L4.34189 12.7913C4.75772 14.205 6.01862 15.2381 7.53438 15.2381C9.02331 15.2381 10.2708 14.2322 10.7134 12.8593H18.0508C18.48 14.2322 19.7409 15.2381 21.2299 15.2381C22.8261 15.2381 24.1541 14.0963 24.4894 12.5874L25.7906 12.1524C26.7161 11.8534 27.3332 10.9834 27.3332 10.0183V8.44144C27.3332 7.01415 26.3674 5.79075 24.9857 5.46451ZM16.1326 2.66429L18.0374 4.43142H12.1756V1.84869H14.0267C14.8047 1.84869 15.5558 2.13415 16.1326 2.66429ZM7.02465 2.94975C7.46731 2.25649 8.23189 1.84869 9.05014 1.84869H10.3513V4.43142H6.07227L7.02465 2.94975ZM7.54779 13.403C6.70272 13.403 6.01862 12.6961 6.01862 11.8398C6.01862 10.9698 6.70272 10.2765 7.54779 10.2765C8.39286 10.2765 9.07696 10.9834 9.07696 11.8398C9.07696 12.6961 8.37945 13.403 7.54779 13.403ZM21.2299 13.403C20.3848 13.403 19.7007 12.6961 19.7007 11.8398C19.7007 10.9698 20.3848 10.2765 21.2299 10.2765C22.0749 10.2765 22.7591 10.9834 22.7591 11.8398C22.7591 12.6961 22.0749 13.403 21.2299 13.403ZM25.5089 10.0183C25.5089 10.195 25.4016 10.3445 25.2272 10.4125L24.3687 10.6979C23.8992 9.37938 22.6786 8.42785 21.2299 8.42785C19.6605 8.42785 18.3459 9.5425 17.9837 11.0242H10.7805C10.4183 9.5425 9.11721 8.42785 7.54779 8.42785C6.0052 8.42785 4.71747 9.50172 4.32847 10.9426L3.12123 10.8339C2.75906 10.8067 2.49078 10.5076 2.49078 10.1542V7.28601C2.49078 6.95977 2.7054 6.68791 3.02733 6.61994L4.51627 6.26652H20.3043L24.5967 7.25883C25.1467 7.38117 25.5357 7.87053 25.5357 8.44144V10.0183H25.5089Z' fill='%237D899B'/%3E%3C/svg%3E%0A"
               alt=""
-            />
+            /> */}
             <input
               className="nonee"
               type="text"
-              placeholder="Enter your car number"
+              placeholder={
+                first.car
+                  ? "Enter your car number"
+                  : first.bike
+                  ? "Enter your Bike number"
+                  : "Enter your Aadhar number"
+              }
               style={{
                 border: "none",
                 padding: "31px 20px",
