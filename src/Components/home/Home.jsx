56b3e966fbdf833f7f73902ed01f2a12e47e525b
Vaibhav Solanki <Vaibhavdasss@gmail.com>
2022-03-30 10:38:17 +0530

    footer

--- src/Components/home/Home.jsx
+++ src/Components/home/Home.jsx
@@ -1,12 +1,14 @@
 import Intro from "./components/Intro/Intro";
 import Ratings from "./components/Ratings/Ratings";
 import PartnerPolicy from "./components/PartnerPolicy/PartnerPolicy";
+import Footer from "./components/footer/footer";
 export default function Home() {
   return (
     <main>
       <Intro />
       <Ratings />
       <PartnerPolicy />
+      <Footer />
     </main>
   );
 }
